b1b009a2d298bbc940e2f53275c384f2e087b84b
Freazure <2193617255@qq.com>
2024-07-29 11:58:41 +0800

    init config

--- blog-cms/src/util/request.js
+++ blog-cms/src/util/request.js
@@ -4,7 +4,7 @@ import 'nprogress/nprogress.css'
 import {Message} from 'element-ui'
 
 const request = axios.create({
-	baseURL: 'http://localhost:8090/admin/',
+	baseURL: 'http://123.57.172.236:8090/admin/',
 	timeout: 5000
 })
 
