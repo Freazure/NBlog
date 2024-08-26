7e11bdb2025326953a8e2e80a8e68e59a8b9c058
Freazure <2193617255@qq.com>
2024-08-26 16:44:11 +0800

    fix(config): jdbc config

--- blog-view/vue.config.js
+++ blog-view/vue.config.js
@@ -10,5 +10,23 @@ module.exports = {
 				'plugins': '@/plugins'
 			}
 		}
-	}
+	},
+	// webpack-dev-server 相关配置
+	// devServer: { // 设置代理
+	// 	host: '0.0.0.0', //
+	// 	port: 80, //自定义端口
+	// 	https: true, //false关闭https，true为开启
+	// 	open: true, //自动打开浏览器
+	// 	proxy: {
+	// 		[process.env.VUE_APP_BASE_API]: { //同济医院地址
+	// 			target: process.env.VUE_APP_BASE_URL,
+	// 			// 如果要代理 websockets
+	// 			ws: true,
+	// 			changeOrigin: true,
+	// 			pathRewrite: {   //重写路径，这种是没有我们定义的前缀
+	// 				['^' + process.env.VUE_APP_BASE_API]: ''
+	// 			}
+	// 		}
+	// 	}
+	// },
 }
